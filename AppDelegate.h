545bb097fa3041b1e49d81dd76b2c00fcc407464
QuanTH <hongquanis@yahoo.com.vn>
2013-09-06 11:25:21 +0700

    Update Source
    nothing

---
+++ AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  RestKitExample
+//
+//  Created by QuanTH on 9/6/13.
+//  Copyright (c) 2013 QuanTH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  RestKitExample
+//
+//  Created by QuanTH on 9/6/13.
+//  Copyright (c) 2013 QuanTH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
